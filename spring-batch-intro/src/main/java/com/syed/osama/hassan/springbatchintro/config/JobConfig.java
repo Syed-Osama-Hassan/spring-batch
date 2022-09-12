cd3cc7dc4cb9e5d24d6d4f4df65b51645e722130
Syed Osama Hassan <syedosamahassan16@gmail.com>
2022-09-12 16:08:47 +0500

    Added incrementer to make job parameters unique

--- spring-batch-intro/src/main/java/com/syed/osama/hassan/springbatchintro/config/JobConfig.java
+++ spring-batch-intro/src/main/java/com/syed/osama/hassan/springbatchintro/config/JobConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -28,6 +29,7 @@ public class JobConfig {
     @Bean
     public Job firstJob() {
         return jobBuilderFactory.get("First Job")
+                .incrementer(new RunIdIncrementer())
                 .start(getFirstStep())
                 .next(getSecondStep())
                 .build();
@@ -53,13 +55,4 @@ public class JobConfig {
         };
     }
 
-//    private Tasklet getSecondTask() {
-//        return new Tasklet() {
-//            @Override
-//            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) {
-//                System.out.println("This is our second tasklet step.");
-//                return RepeatStatus.FINISHED;
-//            }
-//        };
-//    }
 }
