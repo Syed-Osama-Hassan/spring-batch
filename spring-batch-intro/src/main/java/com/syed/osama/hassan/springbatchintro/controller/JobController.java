96ee2f4c113e960d3e7a6e247ca2a3cfbe00d311
Syed Osama Hassan <syedosamahassan16@gmail.com>
2022-09-14 16:26:01 +0500

    Added endpoint to stop the job

--- spring-batch-intro/src/main/java/com/syed/osama/hassan/springbatchintro/controller/JobController.java
+++ spring-batch-intro/src/main/java/com/syed/osama/hassan/springbatchintro/controller/JobController.java
@@ -1,7 +1,11 @@
 package com.syed.osama.hassan.springbatchintro.controller;
 
 import com.syed.osama.hassan.springbatchintro.service.JobService;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +18,23 @@ public class JobController {
     @Autowired
     private JobService jobService;
 
+    @Autowired
+    private JobOperator jobOperator;
+
     @GetMapping("/start/{jobName}")
     public String startJob(@PathVariable String jobName) {
         jobService.startJob(jobName);
         return "Job Started!";
+    }
+
+    @GetMapping("/stop/{jobExecutionId}")
+    public String stopJob(@PathVariable long jobExecutionId) {
+        try {
+            jobOperator.stop(jobExecutionId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return "Job Stopped!";
     }
 }
