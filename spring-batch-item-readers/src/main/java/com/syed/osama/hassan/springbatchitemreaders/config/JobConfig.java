60104d298c2c59de53a51c3e515af7a9a04dca28
Syed Osama Hassan <syedosamahassan16@gmail.com>
2022-09-16 12:55:53 +0500

    Added Flat file Item reader

--- spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/config/JobConfig.java
+++ spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/config/JobConfig.java
@@ -1,5 +1,6 @@
 package com.syed.osama.hassan.springbatchitemreaders.config;
 
+import com.syed.osama.hassan.springbatchitemreaders.model.StudentCsv;
 import com.syed.osama.hassan.springbatchitemreaders.processor.FirstItemProcessor;
 import com.syed.osama.hassan.springbatchitemreaders.reader.FirstItemReader;
 import com.syed.osama.hassan.springbatchitemreaders.writer.FirstItemWriter;
@@ -8,9 +9,18 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 @Configuration
 public class JobConfig {
@@ -40,11 +50,38 @@ public class JobConfig {
 
     private Step firstChunkStep() {
         return stepBuilderFactory.get("First chunk step")
-                .<Integer, Long>chunk(3)
-                .reader(firstItemReader)
-                .processor(firstItemProcessor)
+                .<StudentCsv, StudentCsv>chunk(3)
+                .reader(flatFileItemReader())
+//                .processor(firstItemProcessor)
                 .writer(firstItemWriter)
                 .build();
+    }
+
+    public FlatFileItemReader<StudentCsv> flatFileItemReader() {
+        FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new FileSystemResource(
+                new File("C:\\Users\\OsamaHassan\\Desktop\\development\\practise\\spring batch\\repo\\spring-batch-item-readers\\src\\main\\resources\\input_files\\students.csv")
+        ));
+
+        flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>(){
+            {
+                setLineTokenizer(new DelimitedLineTokenizer() {
+                    {
+                        setNames("ID", "First Name", "Last Name", "Email");
+                    }
+                });
+
+                setFieldSetMapper(new BeanWrapperFieldSetMapper<StudentCsv>(){
+                    {
+                        setTargetType(StudentCsv.class);
+                    }
+                });
+            }
+        });
+
+        flatFileItemReader.setLinesToSkip(1);
+
+        return flatFileItemReader;
     }
 
 }
--- spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/writer/FirstItemWriter.java
+++ spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/writer/FirstItemWriter.java
@@ -1,15 +1,16 @@
 package com.syed.osama.hassan.springbatchitemreaders.writer;
 
+import com.syed.osama.hassan.springbatchitemreaders.model.StudentCsv;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class FirstItemWriter implements ItemWriter<Long> {
+public class FirstItemWriter implements ItemWriter<StudentCsv> {
 
     @Override
-    public void write(List<? extends Long> list) throws Exception {
+    public void write(List<? extends StudentCsv> list) throws Exception {
         System.out.println("Inside Item Writer");
         list.forEach(System.out::println);
     }
