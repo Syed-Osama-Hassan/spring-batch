bfad24eb2409b08b6ccbbbb331fc602abb632140
Syed Osama Hassan <syedosamahassan16@gmail.com>
2022-09-22 17:17:58 +0500

    Added listener to capture bad records while processing and writing from file

--- spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/listener/SkipListener.java
+++ spring-batch-item-readers/src/main/java/com/syed/osama/hassan/springbatchitemreaders/listener/SkipListener.java
@@ -1,6 +1,10 @@
 package com.syed.osama.hassan.springbatchitemreaders.listener;
 
+import com.syed.osama.hassan.springbatchitemreaders.model.StudentCsv;
+import com.syed.osama.hassan.springbatchitemreaders.model.StudentJson;
+import org.springframework.batch.core.annotation.OnSkipInProcess;
 import org.springframework.batch.core.annotation.OnSkipInRead;
+import org.springframework.batch.core.annotation.OnSkipInWrite;
 import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +17,16 @@ public class SkipListener {
             FlatFileParseException ex = (FlatFileParseException) t;
             System.out.println("RECORD: " + ex.getInput());
         }
+    }
+
+    @OnSkipInProcess
+    public void skipInProcess(StudentCsv studentCsv, Throwable t) {
+        System.out.println(studentCsv.toString());
+    }
+
+    @OnSkipInWrite
+    public void skipInWrite(StudentJson studentJson, Throwable t) {
+        System.out.println(studentJson.toString());
     }
 
 }
